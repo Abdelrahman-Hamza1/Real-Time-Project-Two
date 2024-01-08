446958346eada4de678d305940acc2f29e449c28
Nasralla Hassan <122102030+Nsralla@users.noreply.github.com>
2024-01-08 13:54:56 +0200

    Update customer.c

--- customer.c
+++ customer.c
@@ -1,11 +1,6 @@
 #include "local.h"
 // still no ready to execute
-void initialize_storage(int [],int,int);
-void initialize_shelves(int [], int , int);
-void read_Shelf_file(int [], int);
-void customerBuy(int [], int, int, int);
-void checK_shelf(int item_index, int num_items_to_buy, int NUMOFPRODUCTS);
-int main(){
+int main(int argc, char *arg[]){
 
    // to read the configurations (constans) from the file
     int supermarket_config[CONFIG_SIZE];
@@ -15,8 +10,8 @@ int main(){
     int MAX_ITEM_PER_CUSTOMER = supermarket_config[8];
 
     int itemsOnShelf[NUMOFPRODUCTS];  
+    sem_t *sem  = sem_open(SEM_NAME,0);
 
-    sem_t *sem = sem_open(SEM_NAME, 0);
     if(sem == SEM_FAILED){
         perror("sem_open");
         exit(EXIT_FAILURE);
@@ -24,17 +19,18 @@ int main(){
 
     sem_wait(sem);
 
-    FILE *file = fopen(SHELF_FILE, "r");
+    FILE *file = fopen(SHELF_FILE, "r+");
     if ( file == NULL){
         perror("fopen");
         exit(EXIT_FAILURE);
     }
     for(int i =0 ;i<NUMOFPRODUCTS;i++){
         if(fscanf(file,"%d", &itemsOnShelf[i]) != 1){
-            printf(" IN CUSTOMER, Failed to read item %d.",i);
+            printf(" IN CUSTOMER, Failed to read item %d.\n",i);
          }
     }
-    fclose(file);
+
+    
 
     srand(time(NULL));
 
@@ -43,15 +39,24 @@ int main(){
         int itemIndex = randBetween(0, NUMOFPRODUCTS);
         int quantity =  MAX_ITEM_PER_CUSTOMER > itemsOnShelf[itemIndex] ? randBetween(1, itemsOnShelf[itemIndex]) : randBetween(1, MAX_ITEM_PER_CUSTOMER);
         printf("CUSTOMER CHOSE TO BUY  ITEM{%d}  , with quantity = {%d}\n",itemIndex+1,quantity);
+        // update the array
+        itemsOnShelf[itemIndex] = itemsOnShelf[itemIndex] - quantity ;
     }
-   
 
+     // update the file
+    rewind(file);// return the pointer to the start of the file
+    for(int i =0 ;i<NUMOFPRODUCTS;i++){
+        fprintf(file,"%d\n", itemsOnShelf[i]);  
+    }
+
+    fclose(file);
 
     // Release & End Connection with Semaphore
     sem_post(sem);
     sem_close(sem);
-
+     
     sleep(3);
+    
 
     kill(getppid(),SIGUSR1 ); // after we finish, signal to parent!
     return 1;
